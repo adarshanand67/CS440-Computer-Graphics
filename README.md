6365212b5aa89f48160650be224a85318dd2bc80
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2022-01-18 13:42:44 +0530

    Initial commit
    Delete .gitattributes
    
    .
    
    .

---
+++ README.md
@@ -0,0 +1,9 @@
+# CS440 - Computer Graphics using PyOpenGL
+
+- Computer Graphics is a field of study that uses computers to render images.
+
+pip install glfw
+pip install pyopengl
+pip install pyrr
+pip install pillow
+
---
+++ run.sh
@@ -0,0 +1,9 @@
+
+file=$1
+out=$(basename "$file" ".cpp")
+if [ ! -d ".compiled" ] 
+then mkdir ".compiled"
+fi
+export MESA_GL_VERSION_OVERRIDE=4.5 
+g++ "$file" -o ".compiled/$out" -lglut -lGLU -lGL -lGLEW 
+./".compiled/$out"
