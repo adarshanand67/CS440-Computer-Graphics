a6a64431624e5a59bb53cb28a6fa9071b36c51c8
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2022-04-21 20:56:21 +0530

    .

--- README.md
+++
@@ -1,9 +0,0 @@
-# CS440 - Computer Graphics using PyOpenGL
-
-- Computer Graphics is a field of study that uses computers to render images.
-
-pip install glfw
-pip install pyopengl
-pip install pyrr
-pip install pillow
-
--- run.sh
+++
@@ -1,9 +0,0 @@
-
-file=$1
-out=$(basename "$file" ".cpp")
-if [ ! -d ".compiled" ] 
-then mkdir ".compiled"
-fi
-export MESA_GL_VERSION_OVERRIDE=4.5 
-g++ "$file" -o ".compiled/$out" -lglut -lGLU -lGL -lGLEW 
-./".compiled/$out"
